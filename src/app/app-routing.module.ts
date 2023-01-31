2ecc12a36a33d40fcbc7eacf3903f0bdb2a843e3
kienloi10 <kienloi10@gmail.com>
2023-01-31 22:12:37 +0700

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: 'list',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: "list",
+  //   data: {
+  //     breadcrumb: 'Sản phẩm'
+  //   },
+  //   component: ProductListComponent
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
